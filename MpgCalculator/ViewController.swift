5b6753bff28b6dcce48b25570e755ec65e37195e
John Franklin <johnfranklin@johns-mac.local>
2016-09-22 12:52:19 +0100

    Initial Commit

---
+++ MpgCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MpgCalculator
+//
+//  Created by John Franklin on 22/09/2016.
+//  Copyright © 2016 John Franklin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MpgCalculatorTests/MpgCalculatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  MpgCalculatorTests.swift
+//  MpgCalculatorTests
+//
+//  Created by John Franklin on 22/09/2016.
+//  Copyright © 2016 John Franklin. All rights reserved.
+//
+
+import XCTest
+@testable import MpgCalculator
+
+class MpgCalculatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
